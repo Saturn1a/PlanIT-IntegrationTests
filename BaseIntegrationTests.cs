e4caccf24349640febf3edb652a4c4d69c7947d4
tiamaro <tina.olsen@gokstadakademiet.no>
2024-03-19 14:46:39 +0100

    first commit

---
+++ BaseIntegrationTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PlanIT.API.Services.Interfaces;
+
+
+namespace PlanITAPI.IntegrationTests.Docker;
+
+// Fixture-klasse (Base-testklasse som andre tester kan arve fra)
+public class BaseIntegrationTests : IClassFixture<PlanITWebAppFactory>, IDisposable
+{
+    private readonly IServiceScope _serviceScope;
+
+    public BaseIntegrationTests(PlanITWebAppFactory factory)
+    {
+        _serviceScope = factory.Services.CreateScope();
+        Client = factory.CreateClient();
+
+        // Tar inn for å teste mot service
+        UserService = _serviceScope.ServiceProvider.GetService<IUserService>();
+    }
+
+    public HttpClient Client { get; init; }
+
+    public IUserService? UserService { get; init; }
+
+    public void Dispose()
+    {
+        Client?.Dispose();
+    }
+}
---
+++ PlanITWebAppFactory.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using PlanIT.API.Data;
+using Testcontainers.MySql;
+
+namespace PlanITAPI.IntegrationTests.Docker;
+
+public class PlanITWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+
+    private readonly MySqlContainer _mySqlContainer;
+
+    public PlanITWebAppFactory()
+    {
+        _mySqlContainer = new MySqlBuilder()
+            .WithImage("tinamao/planit-db")
+            .WithDatabase("planit_db")
+            .WithUsername("planit-user")
+            .WithPassword("5ecret-plan1t")
+            .Build();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            // først ta bort DbContextOptions
+            var descriptor = services.FirstOrDefault(s => s.ServiceType == typeof(DbContextOptions<PlanITDbContext>));
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            // Setter opp DbContext på nytt
+            services.AddDbContext<PlanITDbContext>(options =>
+            {
+                options.UseMySql(
+                    _mySqlContainer.GetConnectionString(),
+                    new MySqlServerVersion(new Version(8, 0, 33)),
+                    builder =>
+                    {
+                        builder.EnableRetryOnFailure();
+                    });
+            });
+        });
+    }
+
+    // Innebygd interface
+    // STARTER CONTAINER
+    public async Task InitializeAsync()
+    {
+        await _mySqlContainer.StartAsync();
+    }
+
+    // STOPPER CONTAINER
+    public new async Task DisposeAsync()
+    {
+        await _mySqlContainer.StopAsync();
+    }
+
+}
